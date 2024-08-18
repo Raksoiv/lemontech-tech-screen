358e395f197dfbe2e7a685e58e515a338fd3f02e
Oscar Rencoret <o.rencoret@gmail.com>
2024-08-18 00:16:14 -0400

    Move implementation to service file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,31 +1,13 @@
 import { Controller, Get, Request } from '@nestjs/common';
 import { liveScoreConstants } from './constants';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @Get('search')
   async search(@Request() req: any) {
-    const url = `${liveScoreConstants.SEARCH_API}?query=${req.query.q}&limit=10&locale=en&countryCode=CL&categories=true&stages=true&teams=true`;
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    const result: { [k: string]: any } = {
-      Teams: [],
-    };
-    const data = await response.json();
-
-    data.Teams.forEach((team: any) => {
-      result.Teams.push({
-        path: team.ID,
-        name: team.Nm,
-        type: 'team',
-      });
-    });
-
-    return result;
+    return this.appService.search(req.query.q);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { liveScoreConstants } from './constants';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async search(q: string) {
+    const url = liveScoreConstants.SEARCH_API_GEN(q);
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const result: { [k: string]: any } = {
+      Teams: [],
+    };
+    const data = await response.json();
+
+    for (const team of data.Teams) {
+      result.Teams.push({
+        path: team.ID,
+        name: team.Nm,
+        type: 'team',
+      });
+    }
+
+    return result;
   }
 }
